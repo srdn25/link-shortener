1f90adba65367e2cc34f991ff30e461d8a4a45ba
srdn <srdn2417@gmail.com>
2020-03-27 16:31:26 +0200

    Add docker, also make dynamic db config builder

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const logger = require('./logger');
 
+// init update config file for postgres
+require('./postgres');
+
 module.exports = {
   db: {
     daysBeforeLinkExpired: process.env.LINK_EXPIRED_DAYS || 7,
---
+++ src/config/postgres.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = () => {
+  const options = {
+    'development': {
+      'username': process.env.PSQL_USER || 'postgres',
+      'password': process.env.PSQL_PASSWORD || null,
+      'database': process.env.PSQL_DATABASE || 'postgres',
+      'host': process.env.PSQL_HOST || '127.0.0.1',
+      'dialect': 'postgres',
+      'operatorsAliases': false
+    },
+    'test': {
+      'username': 'root',
+      'password': null,
+      'database': 'database_test',
+      'host': '127.0.0.1',
+      'dialect': 'postgres',
+      'operatorsAliases': false
+    },
+    'production': {
+      'username': process.env.PSQL_USER,
+      'password': process.env.PSQL_PASSWORD,
+      'database': process.env.PSQL_DATABASE,
+      'host': process.env.PSQL_HOST,
+      'dialect': 'postgres',
+      'operatorsAliases': false
+    }
+  };
+
+  fs.writeFileSync(path.join(__dirname, '../psql/config.json'), JSON.stringify(options));
+};
