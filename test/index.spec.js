814f9d8d660ad5fcb5e9b9a89cd92340f287ad1f
srdn <srdn2417@gmail.com>
2020-03-29 18:19:36 +0300

    Config tests, install package-all for import all test files

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,31 @@
 const { expect } = require('chai');
 
+const config = require('../src/config');
+const { sequelize } = require('../src/psql/models');
+
+global.config = config;
+
+before(async () => {
+  await sequelize.sync({ force: true });
+});
+
+beforeEach(async () => {
+  const tableNames = Object.keys(sequelize.models);
+  await sequelize.query(
+    `TRUNCATE ${tableNames.map(name => `"${name}"`).join(', ')} CASCADE;`
+  );});
+
+after(async () => {
+  await sequelize.close();
+});
+
+require('require-all')({
+  dirname: __dirname,
+  filter: /spec\.js$/i,
+  recursive: true,
+});
+
+
 describe('Test', function () {
   it('chai should work', () => {
     const a = true;
