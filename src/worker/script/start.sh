383dc0ce789bd9d4a242f02702ae28984271ef3d
srdn <srdn2417@gmail.com>
2020-04-04 13:51:44 +0300

    Add waiting bash script for docker. Wait postgres before start migrations

--- src/worker/script/start.sh
+++ src/worker/script/start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+apk add --no-cache bash
+
+bash ./src/worker/script/waitForIt/main.sh 127.0.0.1:5432 -- echo " === Postgres is available === "
 node ./src/config/postgres.js
 npm run db:migrate
 
