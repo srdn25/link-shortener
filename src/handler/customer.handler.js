e636a1676d70c9812fc8cbedb5d6a648ac0a8523
srdn <srdn2417@gmail.com>
2020-04-08 00:11:44 +0300

    Remove DTO from customer handler

--- src/handler/customer.handler.js
+++ src/handler/customer.handler.js
@@ -1,9 +1,7 @@
 const service = require('../service/customer.service');
-const dto = require('../dto');
 
 module.exports = {
-  create: async ({ email, password, name }) => {
-    const newCustomer = await service.create({ email, password, name });
-    return dto.customer.get(newCustomer);
+  create: async (customer) => {
+    return service.create(customer);
   }
 };
