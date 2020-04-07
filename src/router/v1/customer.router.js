c862a22d7ba360148099b98798b1b2cfa4f70e5f
srdn <srdn2417@gmail.com>
2020-04-07 23:38:44 +0300

    Customer router CREATE

--- src/router/v1/customer.router.js
+++ src/router/v1/customer.router.js
@@ -1,4 +1,10 @@
+const { customer: handler } = require('../../handler');
+
 module.exports = (server, opts, done) => {
   server.get('/ping', async () => ({ response: 'Pong' }));
+
+  server.post('/', (data) => {
+    return handler.create(data);
+  });
   done();
 };
