8ed823876b93cef4e631638904aaff8c38ee5adf
srdn <srdn2417@gmail.com>
2020-04-08 22:40:30 +0300

    Move functional findOrCreate for auth, from repository to service

--- src/repository/customer.repository.js
+++ src/repository/customer.repository.js
@@ -7,15 +7,13 @@ module.exports = {
   findByEmail: (email) => {
     return psql.customer.findOne({ where: { email } });
   },
+  findByGoogleId: (googleId) => {
+    return psql.customer.findOne({ where: { googleId } });
+  },
   updateById: (id, fields) => {
     return psql.customer.update(fields, { where: { id } });
   },
-  findOrCreateAuth: function ({ googleId, email }, cb) {
-    return psql.customer.findOrCreate({ googleId }, async (err, customer) => {
-      if (!customer.email) {
-        await this.updateById(customer.id, { email });
-      }
-      return cb(err, customer);
-    });
+  findOrCreate: (googleId) => {
+    return psql.customer.findOrCreate({ where: { googleId } });
   },
 };
--- src/service/customer.service.js
+++ src/service/customer.service.js
@@ -2,4 +2,17 @@ const { customer: repository } = require('../repository');
 
 module.exports = {
   create: (customer) => repository.create(customer),
+  findOrCreateAuth: function ({ googleId, email }, cb) {
+    return repository.findOrCreate(googleId)
+      .then(async (customer) => {
+        if (!customer.email) {
+          await repository.updateById(customer.id, { email });
+        }
+
+        return cb(null, customer);
+      })
+      .catch((err) => {
+        return cb(err, null);
+      });
+  },
 };
